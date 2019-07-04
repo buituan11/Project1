058dceddcbe646b2eab8aad69f09de305834fab9
buituan11 <buituan71099@gmail.com>
2019-07-04 21:34:08 +0700

    add init file

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from '../Header/header.js';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <Header/>
+    </div>      
+  );
+}
+
+export default App;
