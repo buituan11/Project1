b6bb2c12f8263267b1f398c527a739eb67b1a56c
buituan11 <buituan71099@gmail.com>
2019-07-05 22:54:54 +0700

    update visual

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/header.jsx';
-
+import Visual from '../Visual/visual';
 function App() {
   return (
     <div className="App container-fluid">
       <Header/>
-      <div style={{backgroundColor: 'green', height: '400px'}}>
+      <Visual/>
+      {/*<div style={{backgroundColor: 'green', height: '400px'}}>
       </div>
       <div style={{backgroundColor: 'yellow', height: '400px'}}>
       </div>
       <div style={{backgroundColor: 'blue', height: '400px'}}>
-      </div>
+      </div>*/}
     </div>      
   );
 }
